8d20620d6e4cbf8d0171f45787b89494b0c49879
Michael Carillon <mcarillon97@gmail.com>
2025-02-27 23:05:37 -0500

    feat: install Lucide

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,8 @@
 <template>
   <h1 class="font-bold text-2xl">Hello world!</h1>
-  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-primary"><Music />Button</button>
 </template>
+
+<script setup lang="ts">
+import { Music } from 'lucide-vue-next';
+</script>
