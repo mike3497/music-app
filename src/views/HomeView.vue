4048f953697be59722cad1a858b23048553ce876
Michael Carillon <mcarillon97@gmail.com>
2025-02-27 22:59:53 -0500

    chore: remove boilerplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello world!</h1>
 </template>
