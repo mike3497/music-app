2179f3c43cbfc5d543b457769a5f32305dbd0140
Michael Carillon <mcarillon97@gmail.com>
2025-03-06 22:33:13 -0500

    feat: add error message to SignInView

--- src/views/auth/SignInView.vue
+++ src/views/auth/SignInView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container mx-auto p-4 h-svh">
     <BaseCard title="Sign In">
+      <BaseAlert v-if="errorMessage" :alertColor="AlertColor.ERROR" :message="errorMessage" />
       <form class="flex flex-col gap-4" @submit.prevent="onSubmit">
         <div class="flex flex-col gap-2">
           <label for="email">Email</label>
@@ -22,18 +23,21 @@
 </template>
 
 <script setup lang="ts">
+import BaseAlert from '@/components/shared/BaseAlert.vue';
 import BaseButton from '@/components/shared/BaseButton.vue';
 import BaseCard from '@/components/shared/BaseCard.vue';
 import BaseInput from '@/components/shared/BaseInput.vue';
 import { useUserStore } from '@/stores/userStore';
+import { AlertColor } from '@/types/alertColor';
 import { ref } from 'vue';
 
 const userStore = useUserStore();
 
 const email = ref<string>('');
 const password = ref<string>('');
+const errorMessage = ref<string | null>(null);
 
 const onSubmit = async () => {
-  await userStore.signIn(email.value, password.value);
+  errorMessage.value = await userStore.signIn(email.value, password.value);
 };
 </script>
