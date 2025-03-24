cc17e73091381356a5276595694ee265d99d9657
Michael Carillon <mcarillon97@gmail.com>
2025-03-23 21:16:24 -0400

    feat: add icons to toasts

--- src/components/shared/BaseToast.vue
+++ src/components/shared/BaseToast.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="toast">
-    <div class="alert alert-success">
+    <div class="alert" :class="toastClass">
+      <component :is="toastIcon" />
       <span>{{ toast.message }}</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { type Toast } from '@/models/toast';
-import { type PropType } from 'vue';
+import { ToastVariant, type Toast } from '@/models/toast';
+import { CircleCheck, CircleX, Info, TriangleAlert } from 'lucide-vue-next';
+import { computed, type Component, type PropType } from 'vue';
 
-defineProps({
+const props = defineProps({
   toast: {
     type: Object as PropType<Toast>,
     required: true,
   },
+});
+
+const toastClass = computed<string | null>(() => {
+  switch (props.toast.variant) {
+    case ToastVariant.ERROR:
+      return 'alert-error';
+    case ToastVariant.INFO:
+      return 'alert-info';
+    case ToastVariant.SUCCESS:
+      return 'alert-success';
+    case ToastVariant.WARNING:
+      return 'alert-warning';
+    default:
+      return null;
+  }
+});
+
+const toastIcon = computed<Component | null>(() => {
+  switch (props.toast.variant) {
+    case ToastVariant.ERROR:
+      return CircleX;
+    case ToastVariant.INFO:
+      return Info;
+    case ToastVariant.SUCCESS:
+      return CircleCheck;
+    case ToastVariant.WARNING:
+      return TriangleAlert;
+    default:
+      return null;
+  }
 });
 </script>
