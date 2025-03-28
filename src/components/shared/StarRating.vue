c74b1380e02c2459eb073fc06ada6fa5ebca86b7
Michael Carillon <mcarillon97@gmail.com>
2025-03-27 20:55:24 -0400

    feat: create star rating component

---
+++ src/components/shared/StarRating.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="n in maxRating" :key="n">
+    <span v-if="n <= rating" class="w-6 h-6 mask mask-star bg-secondary"></span>
+    <span v-else class="w-6 h-6 mask mask-star bg-secondary opacity-20"></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  rating: number;
+  maxRating: number;
+}>();
+</script>
